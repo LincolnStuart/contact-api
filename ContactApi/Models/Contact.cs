aeb9426a2f4053dce2d36b06c6b155947825c9f7
LincolnGama <lincolngama@fanese.edu.br>
2019-07-26 09:27:43 -0300

    Issue: Validate objects on binding

--- ContactApi/Models/Contact.cs
+++ ContactApi/Models/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,14 @@ namespace ContactApi.Models
 {
     public class Contact : BaseModel
     {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
         public string Name { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         public void CopyAtributesFrom(Contact contact)
