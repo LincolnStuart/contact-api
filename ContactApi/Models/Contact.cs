848a69f2b4d54151d416b7b2e1e374c9c2efdb8b
LincolnGama <lincolngama@fanese.edu.br>
2019-07-26 09:39:42 -0300

    Issue:  Issue: Validate objects on binding(+)

--- ContactApi/Models/Contact.cs
+++ ContactApi/Models/Contact.cs
@@ -12,6 +12,7 @@ namespace ContactApi.Models
         [MinLength(3)]
         [MaxLength(100)]
         public string Name { get; set; }
+        [Phone]
         [Required]
         public string PhoneNumber { get; set; }
         [Required]
